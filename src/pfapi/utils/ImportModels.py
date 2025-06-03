97c4bb0a0f2d916d98fa685919098a96853ea564
valenciaga1231 <martin.valencic1999@gmail.com>
2025-06-03 08:59:36 +0200

    add other test networks

--- src/pfapi/utils/ImportModels.py
+++ src/pfapi/utils/ImportModels.py
@@ -1,25 +1,21 @@
 import powerfactory as pf
 
-def import_pfd_file(app: pf.Application):
+def import_pfd_file(app: pf.Application, pfd_file_project: str, pfd_project_name: str) -> pf.DataObject:
     # Get current user folder
     current_user_folder = app.GetCurrentUser()
     if not current_user_folder:
         raise ValueError("Current user folder not found. Please ensure you are logged in to PowerFactory.")
     
     # Create new folder in the current user folder
-    new_folder = current_user_folder.CreateObject("IntFolder", "y_bus_example")
-
-    # Define the path to the PFD file and project string
-    file_39_bus = 'grid_models\\39 Bus New England System.pfd'
-    prj_str_39_bus = '39 Bus New England System'
+    new_folder = current_user_folder.CreateObject("IntFolder", "Y Bus Example")
 
     # Get and set the ComImp command
     ComImp = app.GetFromStudyCase("ComPfdImport")
-    ComImp.SetAttribute("g_file", file_39_bus)
+    ComImp.SetAttribute("g_file", pfd_file_project)
     ComImp.SetAttribute("g_target", new_folder)
     
     project_folder_name = new_folder.GetAttribute("loc_name")
-    project_path = project_folder_name + "\\" + prj_str_39_bus
+    project_path = project_folder_name + "\\" + pfd_project_name
     ComImp.Execute() # type: ignore
 
     # Activate the project
